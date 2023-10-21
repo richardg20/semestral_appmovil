f86c89f5d31a2a365c84bb7043df15b35f38829a
richg <rich.gutierrez@duocuc.cl>
2023-10-20 21:35:03 -0300

    android

--- src/app/camara/camara.page.ts
+++ src/app/camara/camara.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType,CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-camara',
@@ -10,6 +11,26 @@ export class CamaraPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    Camera.checkPermissions()
   }
 
+  async selfie(){
+    
+  debugger;
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    var imageUrl = image.webPath;
+
+    // Can be set to the src of an image now
+      //imageElement.src = imageUrl;
+    
+  }
 }
